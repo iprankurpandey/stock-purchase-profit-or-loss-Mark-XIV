f95c1bd3976a21017bab791bd724cde4813764db
iprankurpandey <pprankur@gmail.com>
2021-08-30 22:58:39 +0530

    app is completed

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-const initialPrice =document.querySelector('#initial-price')
+const initialPrice = document.querySelector('#initial-price')
 const stockQuantity = document.querySelector('#stocks-quantity')
-const currentPrice =document.querySelector('#current-price')
+const currentPrice = document.querySelector('#current-price')
 const submitButton = document.querySelector('#submit')
 const outputBox = document.querySelector('#output-box')
 
 
-function calculateProfitAndLoss(inital,quantity,current){
 
-    if(inital > current){
-        let loss = (inital-current)*quantity;
-        let lossPercentage = (loss/inital)*100
-        console.log("the loss is :" + loss)
-        console.log("the loss is in %  :" + lossPercentage)
 
-    }
-    else if(current> inital){
-        let profit = (current-inital)*quantity;
-        let profitPercentage = (profit/inital)*100
-        console.log("the profit is ",+ profit)
-        console.log(profitPercentage)
-      
-    }
-    else{
+function submitHandler(){
+  let ip =initialPrice.value
+  let qty = stockQuantity.value
+  let cp = currentPrice.value
 
+  calculateProfitAndLoss(ip,qty,cp)
+
+  
+
+}
+
+function calculateProfitAndLoss(inital, quantity, current) {
+
+    if (inital > current) {
+        let loss = (inital - current) * quantity;
+        let lossPercentage = (loss / inital) * 100
+        outputBox.innerText = `the loss is ${loss} and loss percentage is ${lossPercentage}% `
+
+    } else if (current > inital) {
+        let profit = (current - inital) * quantity;
+        let profitPercentage = (profit / inital) * 100
+        outputBox.innerText = `the profit is ${profit} and profit percentage is ${profitPercentage}% `
+
+    } else {
+        outputBox.innerText =`no pain ,no gain`
     }
 }
 
-calculateProfitAndLoss(100,10,50)
+// calculateProfitAndLoss(10,10,100)
 
 
-// submitButton.addEventListener("click",consoleClicked)
+submitButton.addEventListener("click",submitHandler)
